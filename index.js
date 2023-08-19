1c199bcab8b4f34d971bc981396338df5162ffa6
Ulan Jusupov <diz.dostuk@yandex.ru>
2023-08-19 22:03:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const kb = require('./keyboard_buttons');
 const keyboard = require('./keyboard');
 
 const TelegramBot = require('node-telegram-bot-api');
-let token = '1029442842:AAEye11IwuHyYVarnHBXc2Y54NzbUdZ5qys';
+let token = TOKEN;
 
 let bot = new TelegramBot(token, {polling: true});
 
